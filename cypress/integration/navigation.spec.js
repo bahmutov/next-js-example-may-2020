bc91c1065e77412cbedd5be9fbf53f7757bc0454
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-08-05 13:03:32 -0400

    remove all but flake reproduction

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -7,33 +7,9 @@ context('Navigation', () => {
     cy.get('[data-cy="header-link-about"]').click(
       // Use force: true to avoid "detached from DOM" error
       // https://github.com/cypress-io/cypress/issues/5743#issuecomment-622596999
-      { force: true },
+      // { force: true },
     );
     cy.get('main:contains("About")');
 
-    cy.get('[data-cy="header-link-products"]').click(
-      // Use force: true to avoid "detached from DOM" error
-      // https://github.com/cypress-io/cypress/issues/5743#issuecomment-622596999
-      { force: true },
-    );
-    cy.get('main h1:contains("Products")');
-
-    cy.get('[data-cy="header-link-users"]').click(
-      // Use force: true to avoid "detached from DOM" error
-      // https://github.com/cypress-io/cypress/issues/5743#issuecomment-622596999
-      { force: true },
-    );
-    cy.get('main h1:contains("Users")');
-    cy.get('main ul li:first-child a').click(
-      // Use force: true to avoid "detached from DOM" error
-      // https://github.com/cypress-io/cypress/issues/5743#issuecomment-622596999
-      { force: true },
-    );
-    // This SHOULD be the same as the line below,
-    // but the line below was not working with my
-    // dev server
-    // cy.get('main h1').contains('Hamed');
-    cy.get('main h1:contains("Hamed")');
-    cy.get('main > p:first-of-type').contains('id:').contains('1');
   });
 });
