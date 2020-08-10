4552b05a45c79a4aab4612f4032d8f9588896ce2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-08-10 08:45:40 -0400

    more testing

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,15 +1,38 @@
 /// <reference types="cypress" />
 
 context('Navigation', () => {
-  it('can navigate around the website', () => {
+  // it('can navigate around the website', () => {
+  //   cy.visit('http://localhost:3000');
+
+  //   cy.get('[data-cy="header-link-about"]').click();
+  //   cy.get('main:contains("About")');
+
+  //   cy.get('[data-cy="header-link-users"]').click();
+  //   cy.get('main h1:contains("Users")');
+  // });
+
+  // it('can navigate around the website (better)', () => {
+  //   cy.visit('http://localhost:3000');
+
+  //   cy.get('[data-cy="header-link-about"]').click();
+  //   cy.location('pathname').should('match', /\/about$/);
+  //   cy.contains('main h1', 'About').should('be.visible');
+
+  //   cy.get('[data-cy="header-link-users"]').click();
+  //   cy.location('pathname').should('match', /\/users$/);
+  //   cy.contains('main h1', 'Users').should('be.visible');
+  // });
+
+  it('can navigate around the website (final)', () => {
     cy.visit('http://localhost:3000');
 
-    cy.get('[data-cy="header-link-about"]').click(
-      // Use force: true to avoid "detached from DOM" error
-      // https://github.com/cypress-io/cypress/issues/5743#issuecomment-622596999
-      // { force: true },
-    );
-    cy.get('main:contains("About")');
+    cy.get('[data-cy="header-link-about"]').should('be.visible')
+      .click();
+    cy.location('pathname').should('match', /\/about$/)
+    cy.contains('main h1', 'About').should('be.visible');
 
+    cy.get('[data-cy="header-link-users"]').click();
+    cy.location('pathname').should('match', /\/users$/)
+    cy.contains('main h1', 'Users').should('be.visible');
   });
 });
