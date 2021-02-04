617586f4c1cf3ff4a70b5f4730126e8bd0b83e0a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-04 09:14:22 -0500

    add explicit visibility test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,27 +1,41 @@
 /// <reference types="cypress" />
 
 context('Navigation', () => {
-  // it('can navigate around the website', () => {
-  //   cy.visit('http://localhost:3000');
+  it.skip('can navigate around the website', () => {
+    cy.visit('http://localhost:3000');
 
-  //   cy.get('[data-cy="header-link-about"]').click();
-  //   cy.get('main:contains("About")');
+    cy.get('[data-cy="header-link-about"]').click();
+    cy.get('main:contains("About")');
 
-  //   cy.get('[data-cy="header-link-users"]').click();
-  //   cy.get('main h1:contains("Users")');
-  // });
+    cy.get('[data-cy="header-link-users"]').click();
+    cy.get('main h1:contains("Users")');
+  });
 
-  // it('can navigate around the website (better)', () => {
-  //   cy.visit('http://localhost:3000');
+  it('click without an explicit visible assertion', () => {
+    cy.visit('http://localhost:3000');
 
-  //   cy.get('[data-cy="header-link-about"]').click();
-  //   cy.location('pathname').should('match', /\/about$/);
-  //   cy.contains('main h1', 'About').should('be.visible');
+    cy.get('[data-cy="header-link-about"]').click();
+    cy.location('pathname').should('match', /\/about$/);
+  })
 
-  //   cy.get('[data-cy="header-link-users"]').click();
-  //   cy.location('pathname').should('match', /\/users$/);
-  //   cy.contains('main h1', 'Users').should('be.visible');
-  // });
+  it('click with an explicit visible assertion', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('[data-cy="header-link-about"]').should('be.visible').click();
+    cy.location('pathname').should('match', /\/about$/);
+  })
+
+  it('can navigate around the website (better)', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('[data-cy="header-link-about"]').click();
+    cy.location('pathname').should('match', /\/about$/);
+    cy.contains('main h1', 'About').should('be.visible');
+
+    cy.get('[data-cy="header-link-users"]').click();
+    cy.location('pathname').should('match', /\/users$/);
+    cy.contains('main h1', 'Users').should('be.visible');
+  });
 
   it('can navigate around the website (final)', () => {
     cy.visit('http://localhost:3000');
